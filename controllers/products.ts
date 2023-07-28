a7da8f9dacf7cf2e340b6cc7f74d2cfacc7f2a86
Gervin Luide <gervin.luide@khk.ee>
2023-07-28 20:14:19 +0300

    Muudatused

--- controllers/products.ts
+++ controllers/products.ts
@@ -1,29 +1,52 @@
 import { Request, Response, Router } from "express";
-import {Toode} from "../models/Toode";
+import Toode from "../models/Toode";
 
 const router: Router = Router();
 
-let toode: Toode | null = new Toode(1, "Koola", 1.5, true);
+const tooted: Toode[] = [
+    new Toode(1,"Koola", 1.5, true),
+    new Toode(2,"Fanta", 1.0, false),
+    new Toode(3,"Sprite", 1.7, true),
+    new Toode(4,"Vichy", 2.0, true),
+    new Toode(5,"Vitamin well", 2.5, true)
+];
 
-router.get("/toode", (req: Request, res: Response) =>  {
-    res.send(toode)
+router.get("/tooted", (req: Request, res: Response) => {
+    res.send(tooted)
 });
 
-router.get("/kustuta-toode", (req: Request, res: Response) =>  {
-    toode = null;
-    res.send(toode)
-});
-
-router.get("/kustuta-toode2", (req: Request, res: Response) =>  {
-    toode = null;
-    res.send("Edukalt kustutatud");
-});
-
-router.get("/suurenda-hinda", (req: Request, res: Response) =>  {
-    if (toode !== null) {
-        toode.price = toode.price + 1;
+router.delete("/kustuta-toode/:index", (req: Request, res: Response) => {
+    if (/^[0-9]+$/.test(req.params.index)) {
+        tooted.splice(Number(req.params.index),1)
     }
-    res.send(toode);
+    res.send(tooted)
+});
+
+router.delete("/kustuta-toode-variant2/:index", (req: Request, res: Response) => {
+    if (/^[0-9]+$/.test(req.params.index)) {
+        tooted.splice(Number(req.params.index),1);
+        res.send("Toode kustutatud!");
+    } else {
+        res.send("Toode kustutamine ei õnnestunud, sisesta number!");
+    }
+});
+
+router.post("/lisa-toode", (req: Request, res: Response) => {
+    if (/^[0-9]+$/.test(req.body.id) && /^[0-9]+$/.test(req.body.price)) {
+        tooted.push(
+            new Toode(req.body.id, req.body.name, req.body.price, req.body.isActive)
+        )
+    }
+    res.send(tooted)
+});
+
+router.patch("/hind-dollaritesse/:kurss", (req: Request, res: Response) => {
+    if (/^[0-9]+$/.test(req.params.kurss)) {
+        for (let index = 0; index < tooted.length; index++) {
+            tooted[index].price = tooted[index].price * Number(req.params.kurss);
+        }
+    }
+    res.send(tooted)
 });
 
 export default router;
